5725586f8db56f64847a6ef42c143687b013cc4f
Katarzyna Klimiuk <klimiuk.kasia@gmail.com>
2022-01-03 14:50:56 +0100

    max_length in models amended

--- books/models.py
+++ books/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 
 class Autor(models.Model):
-    nazwisko = models.CharField(max_length=30, verbose_name='Autor')
+    nazwisko = models.CharField(max_length=250, verbose_name='Autor')
 
     def __str__(self):
         return self.nazwisko
 
 
 class Book(models.Model):
-    tytuł = models.CharField(max_length=50)
+    tytuł = models.CharField(max_length=250)
     autor = models.ForeignKey(Autor, on_delete=models.DO_NOTHING)
     data_publikacji = models.DateField(help_text='yyyy-mm-dd')
-    numer_isbn = models.CharField(max_length=20, verbose_name='Numer ISBN')
+    numer_isbn = models.CharField(max_length=50, verbose_name='Numer ISBN')
     liczba_stron = models.PositiveSmallIntegerField(default=0)
     link_do_okładki = models.URLField()
     język_publikacji = models.CharField(max_length=20)
